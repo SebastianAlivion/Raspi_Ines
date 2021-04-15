8af4df127281cd2091412a17216144b5d4fdec98
SebastianAlivion <81630450+SebastianAlivion@users.noreply.github.com>
2021-04-15 13:28:20 +0200

    Delete Programs/meoh_prototype directory

--- Programs/meoh_prototype/lib/constants.py
+++
@@ -1,8 +0,0 @@
-TIMESTAMP_FORMAT = '%Y-%m-%d_%H:%M:%S:%f'
-LOC_DATA_MPRLS = './data/MPRLS/'
-LOC_DATA_SGP40 = './data/SGP40/'
-LOC_DATA_COMBINED = './data/combined/'
-LOC_DATA_PRESSURE = './data/pressure/'
-LOC_DATA_STABILITY = './data/stability/'
-
-SGP40_YLIM = (20000, None)
