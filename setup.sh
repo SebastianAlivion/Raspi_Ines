261536dbbe056e0c50d9e79fadba50b9c09d3f7e
SebastianAlivion <81630450+SebastianAlivion@users.noreply.github.com>
2021-04-15 13:25:52 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ cd ~
 pip3 install --upgrade adafruit-python-shell
 
 echo -e "\e[96mBlinka Script"
-wget https://raw.githubusercontent.com/SebastianAlivion/RaspberryPiSetup/main/blinka.py
+wget https://raw.githubusercontent.com/SebastianAlivion/Raspi_Ines/main/blinka.py
 python3 blinka.py
 
 echo -e "\e[95mMatplotlib"
@@ -33,7 +33,7 @@ echo -e "\e[95mMPRLS"
 pip3 install adafruit-circuitpython-mprls
 
 echo -e "\e[96mSGP40"
-pip3 install adafruit-circuitpython-sgp40
+pip3 install adafruit-circuitpython-sgp30
 
 echo -e "\e[95mPID"
 pip3 install simple-pid==0.2.4
@@ -42,7 +42,7 @@ echo -e "\e[96mGUI"
 pip3 install pysimplegui
 
 echo -e "\e[95mPrograms"
-git clone https://github.com/SebastianAlivion/RaspberryPiSetup
+git clone https://github.com/SebastianAlivion/Raspi_Ines
 chown -R pi:pi RaspberryPiSetup
 cd RaspberryPiSetup
 mv Programs /home/pi
